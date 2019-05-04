d1bab2924e2975c5fd39684797c599f3e18f9f18
AndyWang99 <sam.andy.wang@gmail.com>
2019-05-04 00:52:16 -0400

    base code deployed

---
+++ precache-manifest.394d3c7a91056719d9791122b67dcabb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "./robots.txt"
+  },
+  {
+    "revision": "00020367a782f5552e7a8923ca181ce0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ffa7d797f59d77315fc6",
+    "url": "./chunk-vendors.js"
+  },
+  {
+    "revision": "ffa7d797f59d77315fc6",
+    "url": "./chunk-vendors.css"
+  },
+  {
+    "revision": "c3d95a5b383d8d3657d8",
+    "url": "./app.js"
+  },
+  {
+    "revision": "c3d95a5b383d8d3657d8",
+    "url": "./app.css"
+  }
+];
